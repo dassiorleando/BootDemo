3e6ca446e8d76b23d3cfe0899c3ba99ce70d0db1
kamdem <valdesekamdem@gmail.com>
2016-03-26 17:54:45 +0100

    Update and delete consultation entity

--- src/main/java/com/thedevbridge/rest/ConsultationResource.java
+++ src/main/java/com/thedevbridge/rest/ConsultationResource.java
@@ -1,12 +1,17 @@
 package com.thedevbridge.rest;
 
-import com.thedevbridge.model.Consultation;
-import com.thedevbridge.service.ConsultationService;
+import javax.inject.Inject;
+
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
+import com.thedevbridge.model.Consultation;
+import com.thedevbridge.service.ConsultationService;
 
 /**
  * Created by marcel on 3/24/16.
@@ -26,5 +31,15 @@ public class ConsultationResource {
     @RequestMapping(value = "/{page}", method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
     public Page<Consultation> getConsultations(@PathVariable int page) {
     	return consultationService.getConsultations(page);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces= MediaType.APPLICATION_JSON_VALUE)
+    public Consultation updateConsultation(@RequestBody Consultation consultation) {
+        return consultationService.save(consultation);
+    }
+    
+    @RequestMapping(value="/{id}", method = RequestMethod.DELETE, produces= MediaType.APPLICATION_JSON_VALUE)
+    public void removeConsultation(@PathVariable String id) {
+        consultationService.remove(id);
     }
 }
--- src/main/java/com/thedevbridge/service/ConsultationService.java
+++ src/main/java/com/thedevbridge/service/ConsultationService.java
@@ -1,14 +1,13 @@
 package com.thedevbridge.service;
 
-import com.thedevbridge.model.Consultation;
-import com.thedevbridge.model.Patient;
-import com.thedevbridge.repository.ConsultationRepository;
-import com.thedevbridge.repository.PatientRepository;
+import javax.inject.Inject;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import javax.inject.Inject;
+import com.thedevbridge.model.Consultation;
+import com.thedevbridge.repository.ConsultationRepository;
 
 /**
  * Created by marcel on 3/24/16.
@@ -26,4 +25,8 @@ public class ConsultationService {
     public Page<Consultation> getConsultations(int page) {
     	return consultationRepository.findAll(new PageRequest(page, 2));
     }
+    
+    public void remove(String id) {
+		consultationRepository.delete(id);
+	}
 }
